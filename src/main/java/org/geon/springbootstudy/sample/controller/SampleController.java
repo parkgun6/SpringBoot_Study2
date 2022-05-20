b757329bfec51f78ee9c5eb5fd861c95e0996fca
parkgun6 <a01088807068@gmail.com>
2022-05-20 09:59:36 +0900

    initial commit

---
+++ src/main/java/org/geon/springbootstudy/sample/controller/SampleController.java
@@ -0,0 +1,17 @@
+package org.geon.springbootstudy.sample.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+public class SampleController {
+
+    @GetMapping("/sample")
+    public ResponseEntity<String> sampleGet() {
+        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+    }
+}
