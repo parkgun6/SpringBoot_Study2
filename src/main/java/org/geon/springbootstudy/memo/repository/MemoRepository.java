ffefe451f7bba3e53b015a35a6d4dc6af6c5dfd5
parkgun6 <a01088807068@gmail.com>
2022-05-23 13:51:36 +0900

    feat: Respository Tests

---
+++ src/main/java/org/geon/springbootstudy/memo/repository/MemoRepository.java
@@ -0,0 +1,21 @@
+package org.geon.springbootstudy.memo.repository;
+
+import org.geon.springbootstudy.memo.entity.Memo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
+
+    Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
+
+    void deleteMemoByMnoLessThan(Long num);
+
+    @Query(value = "select * from tbl_memo where mno > 0", nativeQuery = true)
+    List<Object[]> getNativeResult();
+}
