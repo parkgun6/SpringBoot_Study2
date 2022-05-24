909fe59642fc6c9c2560d564a07a3b0d5430af2c
parkgun6 <a01088807068@gmail.com>
2022-05-24 17:31:32 +0900

    feat: Regist, GetList Tests

---
+++ src/test/java/org/geon/springbootstudy/guestbook/service/GuestbookServiceTests.java
@@ -0,0 +1,53 @@
+package org.geon.springbootstudy.guestbook.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.geon.springbootstudy.guestbook.dto.GuestbookDTO;
+import org.geon.springbootstudy.guestbook.dto.PageRequestDTO;
+import org.geon.springbootstudy.guestbook.dto.PageResultDTO;
+import org.geon.springbootstudy.guestbook.entity.Guestbook;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+public class GuestbookServiceTests {
+
+    @Autowired
+    GuestbookService guestbookService;
+
+    @Test
+    public void testRegister() {
+
+        GuestbookDTO guestbookDTO = GuestbookDTO.builder()
+                .title("Sample Title....")
+                .content("Sample Content.....")
+                .writer("user0")
+                .build();
+
+        log.info(guestbookService.register(guestbookDTO));
+    }
+
+    @Test
+    public void testList() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = guestbookService.getList(pageRequestDTO);
+
+        log.info("PREV: " + resultDTO.isPrev());
+        log.info("NEXT: " + resultDTO.isNext());
+        log.info("TOTAL: " + resultDTO.getTotalPage());
+        log.info("-----------------------------------------");
+
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            log.info(guestbookDTO);
+        }
+
+        log.info("-----------------------------------------");
+        resultDTO.getPageList().forEach(i -> log.info(i));
+
+    }
+}
