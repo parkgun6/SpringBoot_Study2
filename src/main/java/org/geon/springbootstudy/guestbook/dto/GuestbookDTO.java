127cd4601ef1f2b8744bf59de28ce489cd2af032
parkgun6 <a01088807068@gmail.com>
2022-05-24 17:30:31 +0900

    feat: guestbook dto 생성

---
+++ src/main/java/org/geon/springbootstudy/guestbook/dto/GuestbookDTO.java
@@ -0,0 +1,26 @@
+package org.geon.springbootstudy.guestbook.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GuestbookDTO {
+
+    private Long gno;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+    private LocalDateTime regDate, modDate;
+
+}
