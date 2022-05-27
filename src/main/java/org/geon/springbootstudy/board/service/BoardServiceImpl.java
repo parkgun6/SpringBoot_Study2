aa662e647a325c24b2dab3a5623a6e2b7e39befc
parkgun6 <a01088807068@gmail.com>
2022-05-27 17:42:52 +0900

    feat: search로직 추가

--- src/main/java/org/geon/springbootstudy/board/service/BoardServiceImpl.java
+++ src/main/java/org/geon/springbootstudy/board/service/BoardServiceImpl.java
@@ -47,8 +47,14 @@ public class BoardServiceImpl implements BoardService {
                 (Long)en[2])
         );
 
-        Page<Object[]> result = boardRepository.getBoardWithReplyCount(
-                pageRequestDTO.getPageable(Sort.by("bno").descending()));
+//        Page<Object[]> result = boardRepository.getBoardWithReplyCount(
+//                pageRequestDTO.getPageable(Sort.by("bno").descending()));
+
+        Page<Object[]> result = boardRepository.searchPage(
+            pageRequestDTO.getType(),
+            pageRequestDTO.getKeyword(),
+            pageRequestDTO.getPageable(Sort.by("bno").descending())
+        );
 
         return new PageResultDTO<>(result, fn);
     }
