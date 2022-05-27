336e78345d490091f9d3e5a56cfc14a55035b983
parkgun6 <a01088807068@gmail.com>
2022-05-27 17:45:23 +0900

    feat: 댓글 가져오는 메서드 생성 1.getRepliesByBoardOrderByRno

--- src/main/java/org/geon/springbootstudy/board/repository/ReplyRepository.java
+++ src/main/java/org/geon/springbootstudy/board/repository/ReplyRepository.java
@@ -1,10 +1,13 @@
 package org.geon.springbootstudy.board.repository;
 
+import org.geon.springbootstudy.board.entity.Board;
 import org.geon.springbootstudy.board.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
@@ -12,4 +15,6 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
     @Query("delete from Reply r " +
             "where r.board.bno = :bno")
     void deleteByBno(@Param("bno") Long bno);
+
+    List<Reply> getRepliesByBoardOrderByRno(Board board);
 }
